ea094b017481185b2b0f398ef01136ef6981ca56
Rmoreno3 <reynaldomoreno173@gmail.com>
2022-11-30 18:26:26 -0400

    Creating formLogin

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,9 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, Button, Keyboard, StyleSheet } from 'react-native'
 
 export default function LoginForm () {
   return (
     <View>
-      <Text>LoginForm</Text>
+      <Text style={styles.title}>Iniciar Sesion</Text>
+      <TextInput
+        placeholder='Username'
+        style={styles.input}
+        autoCapitalize='none'
+      />
+      <TextInput
+        placeholder='Password'
+        style={styles.input}
+        secureTextEntry
+        autoCapitalize='none'
+      />
+      <Button
+        title='Login'
+        onPress={() => console.log('Login')}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginBottom: 15,
+    textAlign: 'center'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10
+  }
+})
