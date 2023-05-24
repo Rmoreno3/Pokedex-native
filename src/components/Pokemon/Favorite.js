6acaeea23ad789c81f7db86bf4764801d9d14c55
Reynaldo Moreno <reynaldomoreno173@gmail.com>
2023-05-23 21:36:12 -0400

    agregando pack asyncStorage y creando servicio para almacenar pokemons favoritos

--- src/components/Pokemon/Favorite.js
+++ src/components/Pokemon/Favorite.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Icon from '@expo/vector-icons/FontAwesome5'
+import { addPokemonFavorite } from '../../api/favorite'
 
 export default function Favorite(props) {
   const { id } = props
-  const addFavorite = () => {
-    console.log('Adding pokemon...', id)
+
+  const addFavorite = async () => {
+    await addPokemonFavorite(id)
   }
 
   return <Icon name="heart" color="#fff" size={20} onPress={addFavorite} />
--- src/screens/FavoriteScreen.js
+++ src/screens/FavoriteScreen.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { View, Text, Button } from 'react-native'
+import { getPokemonFavorite } from '../api/favorite'
 
-export default function Favorites () {
+export default function Favorites() {
+  const [favorites, setFavorites] = useState([])
+
+  const checkFavorites = async () => {
+    const response = await getPokemonFavorite()
+    console.log(response)
+  }
+
   return (
     <View>
       <Text>Favorites</Text>
+      <Button title="Get favorites" onPress={checkFavorites} />
     </View>
   )
 }
